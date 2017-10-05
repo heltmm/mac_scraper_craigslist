525e54ee6adae2d4820106c6cf75ed232e38ea01
Mark <heltmm@gmail.com>
2017-10-04 23:20:25 -0700

    add basic table to show returned macbook results

--- app.rb
+++ app.rb
@@ -1,40 +1,51 @@
 require("bundler/setup")
 require 'open-uri'
 require 'pry'
-
-
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
-parse_file = Nokogiri::HTML(open('https://portland.craigslist.org/search/sya?query=macbook+pro'))
-search_links = parse_file.xpath("//a[@class='result-title hdrlnk']/@href")
-
-keywords = ['broken', 'damaged', 'parts', 'cracked', 'crack', 'not working', 'damage', 'fix']
-macs = []
-
-search_links.each do |link|
-  page = Nokogiri::HTML(open(link).read)
-
-  link = link.text
-  condition = page.xpath("//p[@class='attrgroup']/span[1]/b").text
-  manufacturer = page.xpath("//p[@class='attrgroup']/span[2]/b").text
-  model = page.xpath("//p[@class='attrgroup']/span[3]/b").text
-  title = page.xpath("//span[@id='titletextonly']").text
-
-  if price = page.xpath("//span[@class='price']").text
-    price = price.gsub("$", "")
-  end
-  description = page.xpath("//section[@id='postingbody']").text
-  if location = page.xpath("//span[@class='postingtitletext']/small").text
-    if location != ''
-      location = location.chop!.gsub(' ','')[1..-1]
-    end
-  end
-
-  keywords.each do |word|
-    if !title.match(word) and !description.match(word)
-      macs.push({:link => link, :model => model, :manufacturer => manufacturer, :title => title, :price => price, :condition => condition, :description => description, :location => location})
-    end
-  end
+get '/' do
+  @macs = []
+  @damaged_macs = []
+  erb(:index)
 end
-binding.pry
+
+post '/' do
+  #parse local craiglist macbook search
+  parse_file = Nokogiri::HTML(open('https://portland.craigslist.org/search/sss?query=+macbook+pro&sort=rel'))
+  # select every macbook link
+  search_links = parse_file.xpath("//a[@class='result-title hdrlnk']/@href")
+  # keywords to sort broken or damaged
+  keywords = ['broken', 'damaged', 'parts', 'cracked', 'crack', 'not working', 'damage', 'fix']
+
+  @macs = []
+  @damaged_macs = []
+  # parse each macbook page
+  search_links.each do |link|
+    page = Nokogiri::HTML(open(link).read)
+
+    link = link.text
+    condition = page.xpath("//p[@class='attrgroup']/span[1]/b").text
+    manufacturer = page.xpath("//p[@class='attrgroup']/span[2]/b").text
+    model = page.xpath("//p[@class='attrgroup']/span[3]/b").text
+    title = page.xpath("//span[@id='titletextonly']").text
+    # remove extra characters and spaces that were selected along with information
+    if price = page.xpath("//span[@class='price']").text
+      price = price.gsub("$", "")
+    end
+    description = page.xpath("//section[@id='postingbody']").text.gsub("QR Code Link to This Post ", '')
+    if location = page.xpath("//span[@class='postingtitletext']/small").text
+      if location != ''
+        location = location.chop!.gsub(' ','')[1..-1]
+      end
+    end
+
+    if keywords.any? {|word| title.match(word) || description.match(word)}
+      @damaged_macs.push({:link => link, :model => model, :manufacturer => manufacturer, :title => title, :price => price, :condition => condition, :description => description, :location => location})
+    else
+      @macs.push({:link => link, :model => model, :manufacturer => manufacturer, :title => title, :price => price, :condition => condition, :description => description, :location => location})
+    end
+  end
+  binding.pry
+  erb(:index)
+end
