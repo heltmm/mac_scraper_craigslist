e3850bab1b85504342a950d2b8cbc0886e80dd39
Mark Helt <heltmm@gmail.com>
2017-10-22 15:05:00 -0700

    make mac.date_posted.julian an if statement incase missing that information

--- lib/mac.rb
+++ lib/mac.rb
@@ -56,9 +56,10 @@ class Mac < ActiveRecord::Base
     # current date converted to julian
     today = Date.today.julian.strftime("%j").to_i
     current_macs.each do |mac|
-      check = mac.date_posted.julian.strftime("%j").to_i
-      if (today - check) > 30
-        mac.delete
+      if check = mac.date_posted.julian.strftime("%j").to_i
+        if (today - check) > 30
+          mac.delete
+        end
       end
     end
   end
